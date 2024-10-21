11982725ff8b10626a335b3860a2ceea36aec601
MauroDeveloper7 <duqueivan493@gmail.com>
2024-10-21 16:38:56 -0500

    Avances 21/10/2024

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { PrismaService } from './prisma.service';
     }),
     UserModule,
     AuthModule,
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
---
+++ src/product/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class ProductService extends PrismaClient implements OnModuleInit {
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  create(createProductDto: CreateProductDto) {
+    const newUser =this.product
+  }
+
+  findAll() {
+    return `This action returns all product`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
